678ddccf46c1bfe9dd25dbec5c53a58248317284
milenaemmert <milena.emmert@outlook.com>
2024-08-08 09:40:49 -0300

    Semana 5 - Aula 1

--- js/index.js
+++
@@ -1,9 +0,0 @@
-import { listarTarefasNaTela } from './listaDeTarefas.js'
-import { criarPrioridades, pegarDadosDoFormulario, limparFormulario } from './formulario.js'
-import { pegarTarefasDaApi } from './api.js'
-
-criarPrioridades()
-listarTarefasNaTela()
-
-document.querySelector('#formularioTarefas').addEventListener('submit', pegarDadosDoFormulario)
-document.querySelector('#botaoLimpar').addEventListener('click', limparFormulario)
